2a2b602f75fbe1b9976d83bdb0f5f882a441a5b5
alain <alainbloch@gmail.com>
2009-12-16 10:53:06 -0800

    added basic CRUD methods to users controller and added in user auth for the edit/update/destroy pages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  
+  before_filter :get_user, :only => [:show, :edit]
+  before_filter :correct_user_required, :only => [:edit]
+  
   def new
     @user = User.new
   end
@@ -13,4 +17,31 @@ class UsersController < ApplicationController
       render :action => 'new'
     end
   end
+
+  def show
+    # get_user
+  end
+  
+  def edit
+    # get_user
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+private
+  
+  def get_user
+    @user = User.find_by_id(params[:id])
+  end
+  
+  def correct_user_required
+    redirect_to login_url unless @user == current_user
+  end
+
 end
