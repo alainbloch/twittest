fba3177ae220f109c15078c76bd43478777ea2ee
alain <alainbloch@gmail.com>
2009-12-16 23:40:20 -0800

    revised home controller test.

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -8,20 +8,16 @@ class HomeControllerTest < ActionController::TestCase
       setup do
         post :index, {}, {:user_id => users(:foo).id}
       end
-      
-      should_assign_to :feed
+      should_assign_to :messages
       should_render_template :dashboard
-      
     end
     
     context "when not logged in" do
       setup do
         post :index
       end
-      
-      should_not_assign_to :feed
-      should_render_template :index
-            
+      should_assign_to :messages
+      should_render_template :index            
     end
     
   end
