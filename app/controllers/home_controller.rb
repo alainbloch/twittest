4d2f09e8415cea4155a82c1b7cb8dfeb581240e8
alain <alainbloch@gmail.com>
2009-12-16 22:04:03 -0800

    added logic for home index method.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
   
   def index
-    
+    if logged_in?
+      @user = current_user
+      @feed = current_user.feed.paginate(:page => params[:page])
+      @message = Message.new
+      render :template => "/home/dashboard"
+    end
   end
   
 end
