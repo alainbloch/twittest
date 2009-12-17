4cf0af6d97cbb47b758e32dfec9186b4db6e3156
alain <alainbloch@gmail.com>
2009-12-16 18:23:36 -0800

    revised format of username to not include @. Added messages association to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   
   validates_presence_of :username, :full_name
   validates_uniqueness_of :username, :email, :allow_blank => true
-  validates_format_of :username, :with => /^[-\w\._@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
+  validates_format_of :username, :with => /^[-\w\._]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_"
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   validates_presence_of :password, :on => :create
   validates_confirmation_of :password
@@ -22,6 +22,8 @@ class User < ActiveRecord::Base
   # users who are following the user
   has_many :followings
   has_many :followers, :through => :followings
+  
+  has_many :messages
   
   # login can be either username or email address
   def self.authenticate(login, pass)
