75c750b537cd78fb3708b7f94d92ae2ee6a39817
alain <alainbloch@gmail.com>
2009-12-16 23:32:04 -0800

    revised home index to use @messages instead of @feed. added in js support that renders the messages.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,23 @@
 class HomeController < ApplicationController
   
   def index
+    @messages = 
     if logged_in?
-      @user = current_user
-      @feed = current_user.feed.paginate(:page => params[:page])
-      @message = Message.new
-      render :template => "/home/dashboard"
+      current_user.feed.paginate(:page => params[:page])
     else
-      @feed = Message.find(:all, :limit => 20, :order => "created_at ASC")
+      Message.find(:all, :limit => 20, :order => "created_at ASC")
+    end
+    respond_to do |format|
+      format.html do
+        if logged_in?
+          @user = current_user
+          @message = Message.new
+          render :template => "/home/dashboard"
+        end
+      end
+      format.js do
+      render @messages
+      end
     end
   end
   
