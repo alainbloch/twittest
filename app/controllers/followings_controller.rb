075ded5b2da15bc5a8167646d4d71d8ee9fd4802
alain <alainbloch@gmail.com>
2009-12-16 16:27:34 -0800

    added create and destroy logic to followings controller.

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,12 +1,64 @@
 class FollowingsController < ApplicationController
-  before_filter :login_required
+  before_filter :login_required, :get_user, :user_required
   
   def create
-    
+    respond_to do |format|
+      if @user.followings.create(:follower => current_user)
+        message = "You are now following #{current_user.full_name}"
+        format.html do
+          flash[:notice] = message
+          redirect_to user_path(@user)
+        end
+        format.js{render(:update){|page| page.alert message}}
+      else
+        message = "Something went wrong!"
+        format.html do
+          flash[:error] = message
+          redirect_to user_path(@user)
+        end
+        format.js{render(:update){|page| page.alert message}}
+      end
+    end
   end
   
   def destroy
-    
+    respond_to do |format|
+      following = @user.followings.find_by_follower_id(current_user)
+      if not following.nil? and following.destry
+        message = "You are not following #{current_user.full_name} anymore"
+        format.html do
+          flash[:notice] = message
+          redirect_to user_path(@user)
+        end
+        format.js{render(:update){|page| page.alert message}}
+      else
+        message = "Something went wrong!"
+        format.html do
+          flash[:error] = message
+          redirect_to user_path(@user)
+        end
+        format.js{render(:update){|page| page.alert message}}
+      end
+    end
+  end
+  
+private
+
+  def get_user
+    @user = User.find_by_id(params[:user_id])
+  end
+  
+  def user_required
+    if @user.nil?
+      respond_to do |format|
+        message = "User hasn't been selected"
+        format.js {render(:update){|page| page.alert message}}
+        format.html do
+          flash[:error] = message
+          redirect_to :back
+        end
+      end
+    end
   end
   
 end
